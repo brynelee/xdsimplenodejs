ea613ad75a4f6b2c724c695f70217f1442278b0e
Xiaodong Li <bryne_lxd@sina.com>
2019-12-28 00:19:54 +0800

    added web.js forr simple web server by node.js

---
+++ web.js
@@ -0,0 +1,21 @@
+//  web.js
+//  引入http模块
+var http = require('http')
+
+//设置请求监听函数
+/**
+  *  param req:   请求信息
+  *  param res:   响应信息
+  */
+var requestHandler = function (req, res) {
+            res.end('hello');
+    }
+
+//创建服务器
+var web = http.createServer(requestHandler)
+
+//设置监听端口号
+web.listen(8080)
+
+console.log('http running on http://localhost:8080')
+
